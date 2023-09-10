108d38248709d80a46ca56c3734cc6892ffb16ca
Chinmaya Nagpal <chinmaya1011@gmail.com>
2023-09-11 00:57:24 +0100

    add exercise 5.1

--- 5.1.lean
+++ 5.1.lean
@@ -1,3 +1,5 @@
+import data.real.basic
+import data.int.basic 
 -- Exercises from chapter 3
 -- exercise 3.1
 
@@ -160,4 +162,262 @@ begin
   have : p, from h_mpr h,
   contradiction
 end
+end
+
+
+-- chapter 4
+-- exercise 4.1
+section
+variables (α : Type*) (p q : α → Prop)
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := 
+by apply iff.intro; { intro h, simp *}
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := 
+by { intros, simp *}
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := 
+by { intro h, cases h; simp *}
+end
+
+-- exercise 4.2
+section
+variables (α : Type*) (p q : α → Prop)
+variable r : Prop
+
+example : α → ((∀ x : α, r) ↔ r) := 
+begin
+  intro a,
+  apply iff.intro, 
+    assume h, exact h ‹α›, 
+  intros, assumption
+end
+
+section uses_classical
+open classical
+
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := 
+begin
+  apply iff.intro, 
+  begin
+    intro h, by_cases hr: r, 
+      { right, assumption }, 
+      { left, assume x, cases (h x), assumption, contradiction },
+  end,
+  begin
+    intro h, cases h, 
+      { assume x, left, exact h x },
+      { assume x, right, assumption }
+  end
+end
+end uses_classical
+
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := 
+by  apply iff.intro; { intros, simp * }
+end
+
+-- exercise 4.4 : no theorems/examples
+
+-- exercise 4.5
+section
+open classical
+
+variables (α : Type*) (p q : α → Prop)
+variable r : Prop
+
+example : (∃ x : α, r) → r := 
+by { intro h, cases h, assumption }
+example (a : α) : r → (∃ x : α, r) := 
+by { intro h, split; assumption }
+example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := 
+begin
+  apply iff.intro,
+  begin
+    intro h,
+    split; cases h with a ha, 
+    split, exact ha.left, 
+    exact ha.right,
+  end,
+  begin
+    intro h,
+    cases h with hl hr,
+    cases hl with a hla,
+    split, split, assumption, assumption 
+  end
+end
+
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := 
+begin
+  apply iff.intro,
+  begin
+    intro h, 
+    cases h with a ha, 
+    cases ha,
+      left, split, exact ha,
+    right, split, exact ha
+  end,
+  begin
+    intro h,
+    cases h,
+      cases h, split, left, assumption,
+    cases h, split, right, assumption
+  end
+end
+
+example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := 
+begin
+  apply iff.intro,
+  begin
+    intro h,
+    by_contradiction h',
+      cases h' with a hna, exact hna (h a),
+  end,
+  begin
+    intro h,
+    assume x,
+    by_contradiction hnx, 
+      exact h (exists.intro x hnx),
+  end
+end
+example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := 
+begin
+  apply iff.intro,
+  begin
+    intro h,
+    by_contradiction h',
+      cases h with a ha, exact (h' a) ha, 
+  end,
+  begin
+    intro h,
+    by_contradiction h',
+    exact h (assume x : α, assume t: p x, h' (exists.intro x t)) 
+  end
+end
+example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := 
+begin
+apply iff.intro,
+  begin
+    intro,
+    assume x, 
+    assume t,
+    have : ∃ x, p x, 
+    existsi x, exact t, contradiction
+  end,
+  begin
+    intro h,
+    by_contradiction h',
+    cases h' with a h'a,
+    exact (h a) h'a
+  end
+end
+example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := 
+begin
+apply iff.intro,
+  begin
+    intro h, 
+    by_contradiction h',
+    have, from (assume x: α, show p x, from by_contradiction ((assume t : ¬p x,
+    h' (exists.intro x t)))),
+    contradiction
+  end,
+  begin
+    intro h,
+    by_contradiction h', 
+    cases h with a ha,
+    have : p a, from h' a, 
+    contradiction
+  end
+end
+
+example : (∀ x, p x → r) ↔ (∃ x, p x) → r := 
+begin
+apply iff.intro,
+  begin
+    intro h,
+    intro h',
+    cases h' with a h'a, 
+    exact h a h'a 
+  end,
+  begin
+    intro h,
+    assume x, assume t, 
+    have : (∃ x : α, p x), existsi x, exact t,
+    simp *
+  end
+end
+example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := 
+begin
+apply iff.intro,
+  begin
+    intro h, cases h, intro, simp *
+  end,
+  begin
+    intro h₁,
+    by_cases h₂ : (∀ x, p x),
+      have h₃ : p a, from h₂ a,
+      have h₄ : (p a → r), from (assume t : p a, h₁ h₂),
+      show ∃ x, p x → r, existsi a, exact h₄,
+    have h₃ : ∃ x, ¬ p x, by_contradiction h₄,
+    from h₂ begin assume x, show p x, by_contradiction t, exact h₄ ⟨x, t⟩ end,
+    cases h₃ with x hx,
+    have t : p x → r, intro, contradiction, 
+    constructor, assumption 
+  end
+end
+example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := 
+begin
+apply iff.intro,
+  begin
+    intro h, intro t, cases h, existsi h_w, exact h_h t
+  end,
+  begin
+    intro h,
+    by_cases t : r,
+      have, from h t,
+      cases this,
+      existsi this_w,
+      intro, assumption,
+    existsi a, intro, contradiction
+  end
+end
+
+end
+
+-- exercise 4.6
+section
+-- import data.real.basic (top of file)
+
+variables log exp     : real → real
+variable  log_exp_eq : ∀ x, log (exp x) = x
+variable  exp_log_eq : ∀ {x}, x > 0 → exp (log x) = x
+variable  exp_pos    : ∀ x, exp x > 0
+variable  exp_add    : ∀ x y, exp (x + y) = exp x * exp y
+
+-- this ensures the assumptions are available in tactic proofs
+include log_exp_eq exp_log_eq exp_pos exp_add
+
+example (x y z : real) :
+  exp (x + y + z) = exp x * exp y * exp z :=
+by rw [exp_add, exp_add]
+
+example (y : real) (h : y > 0)  : exp (log y) = y :=
+exp_log_eq h
+
+theorem log_mul {x y : real} (hx : x > 0) (hy : y > 0) :
+  log (x * y) = log x + log y :=
+begin
+  rw [←exp_log_eq hx, ←exp_log_eq hy],
+  rw [←exp_add],
+  repeat { rw [log_exp_eq] },
+end
+end
+
+
+-- exercise 4.7
+
+section
+-- import data.int.basic (top of file)
+
+#check sub_self
+
+example (x : ℤ) : x * 0 = 0 :=
+  by simp *
 end
