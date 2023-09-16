93994f42503024c767feea39bc3ff5ad6a0d1a72
Chinmaya Nagpal <chinmaya1011@gmail.com>
2023-09-16 22:48:54 +0100

    add exercise 7.4

--- 7.4.lean
+++ 7.4.lean
@@ -1,9 +1,10 @@
+import data.nat.basic
+
 inductive form 
 | const : bool → form
 | var   : ℕ → form
 | and   : form → form → form
 | or    : form → form → form
-
 
 def evaluate (x : form) (f : ℕ → bool) : bool :=
 begin
@@ -19,5 +20,15 @@ begin
   induction x, 
   case const : x { exact 0},
   case var   : n { exact 0},
-  case and   : f1 f2 cf1 cf2 {  }
+  case and   : f1 f2 cf1 cf2 { exact 1 + (max cf1 cf2) },
+  case or    : f1 f2 cf1 cf2 { exact 1 + (max cf1 cf2) } 
+end
+
+def subst (x : form) (n : ℕ) (y : form) : form :=
+begin
+  induction x, 
+  case const :   { exact form.const x },
+  case var   : m { exact if m = n then y else form.var m},
+  case and   : f1 f2 sf1 sf2  { exact form.and sf1 sf2 },
+  case or    : f1 f2 sf1 sf2  { exact form.or sf1 sf2 }
 end
